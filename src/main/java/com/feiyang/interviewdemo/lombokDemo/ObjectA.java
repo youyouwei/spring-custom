c2c7992b620e914c5d362c409ab4021c4beae717
jiahuiyang <huiyang.jia@zatech.com>
2019-09-26 10:07:54 +0800

    lombok demo

---
+++ src/main/java/com/feiyang/interviewdemo/lombokDemo/ObjectA.java
@@ -0,0 +1,33 @@
+package com.feiyang.interviewdemo.lombokDemo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @Description:
+ * @Author: jiahuiyang
+ * @Date: Created in 10:02 2019/9/25
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+
+public class ObjectA {
+
+    public static void main(String[] args) {
+        ObjectA objectA = new ObjectA();
+        objectA.setAddress("sss");
+        objectA.setName("ddd");
+        objectA.setId(1);
+
+
+        System.out.println(objectA);
+    }
+
+
+
+    private String name;
+    private String address;
+    private Integer id;
+
+}
+
---
+++ src/main/java/com/feiyang/interviewdemo/testdemo/Demo.java
@@ -0,0 +1,14 @@
+package com.feiyang.interviewdemo.testdemo;
+
+/**
+ * @Description:
+ * @Author: jiahuiyang
+ * @Date: Created in 16:39 2019/9/16
+ */
+public class Demo {
+
+    public void method() {
+
+    }
+
+}
