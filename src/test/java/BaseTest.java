00bc9de749a1d1a19050f59ab7c1aa2cd33ff6e3
za-jiahuiyang <huiyang.jia@zatech.com>
2019-10-21 11:47:42 +0800

    add Junit test

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @Description:
+ * @Author: jiahuiyang
+ * @Date: Created in 11:43 2019/10/21
+ */
+public class BaseTest {
+
+    @Test
+    public void method() {
+        System.out.println("base test method");
+    }
+
+}
