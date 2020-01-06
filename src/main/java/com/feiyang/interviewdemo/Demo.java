64f010b7873fb5071e4a24cc5b0fea5351aadb03
za-jiahuiyang <huiyang.jia@zatech.com>
2020-01-06 15:18:07 +0800

    sss

--- src/main/java/com/feiyang/interviewdemo/Demo.java
+++ src/main/java/com/feiyang/interviewdemo/Demo.java
@@ -62,7 +62,7 @@ public class Demo {
 
         int oldCapacity = 15;
         int newCapacity = oldCapacity + (oldCapacity >> 1);
-        System.out.println(newCapacity + "");
+        System.out.println(newCapacity + "jack");
 
         Map<String, String> map = new Hashtable<>();
 
