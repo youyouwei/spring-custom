05b8ee204bee229f45b241c552c1c10ea435e368
youyouwei <286312366@qq.com>
2020-02-25 09:40:21 +0800

    jvm

---
+++ src/main/java/com/feiyang/interviewdemo/jvmDemo/HeapMemoryTest.java
@@ -0,0 +1,27 @@
+package com.feiyang.interviewdemo.jvmDemo;
+
+/**
+ * @Description:
+ *
+ * 设置jvm 堆内存 -Xmx20m -Xms5m
+ * @Author: jiahuiyang
+ * @Date: Created in 9:31 2020/2/22
+ */
+public class HeapMemoryTest {
+    public static void main(String[] args) {
+        byte[] b = new byte[1 * 1024 * 1024];
+        System.out.println("分配了1M空间给数组");
+
+        System.out.print("Xmx=");
+        System.out.println(Runtime.getRuntime().maxMemory() / 1024.0 / 1024 + "M");
+
+//        空闲内存
+        System.out.print("free mem=");
+        System.out.println(Runtime.getRuntime().freeMemory() / 1024.0 / 1024 + "M");
+
+//        总内存 正常是Xms jvm尽可能在最小堆内存大小运行，如果GC后还不能满足，会进行扩容直至Xmx
+        System.out.print("total mem=");
+        System.out.println(Runtime.getRuntime().totalMemory() / 1024.0 / 1024 + "M");
+
+    }
+}
